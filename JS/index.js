cfc1b27d90ee8b86a717ba4a632da2201c99d4a1
ahmed-magdy199600 <ahmed.magdy199600@gmail.com>
2024-03-27 14:16:53 +0200

    till img

--- JS/index.js
+++ JS/index.js
@@ -3,65 +3,130 @@ var productPriceInput = document.getElementById('productPrice');
 var productCategoryInput = document.getElementById('productCategory');
 var productDescriptionInput = document.getElementById('productDescription');
 var productImageInput = document.getElementById('productImage');
-// _______________________________________________________________________________
+var searchInput = document.getElementById('searchInput');
+var addBtn = document.getElementById('addBtn');
+var updateBtn = document.getElementById('updateBtn'); 
+var updatedIndex;
 
+var productContainer = []; 
 
-if(localStorage.getItem("products") == null ) {
-    productContainer = [];
-}
-else {
-    productContainer = JSON.parse ( localStorage.getItem('products'));
-    displayProducts();
-}
-
-
-
-// _______________________________________________________________________________
-
-function addProduct()
+if (localStorage.getItem('products') !==null) 
 {
+    productContainer = JSON.parse( localStorage.getItem('products'));
+    displayProducts(); 
+}
+
+
+function addProduct() {
+
     var product = {
         code:productNameInput.value,
         price:productPriceInput.value,
         category:productCategoryInput.value,
         description:productDescriptionInput.value,
-        image:'images/tv.jpg'
-        }
-        
-        productContainer.push(product);
-        clearForm();
-        displayProducts();
-        localStorage.setItem('products' , JSON.stringify(productContainer));
-
+        image:`img/${productImageInput.files[0]?.name}`
+    }
+    productContainer.push(product);
+    localStorage.setItem("products", JSON.stringify(productContainer));
+    clearForm();
+    displayProducts();
 }
-//_______________________________________________________________________________
-function clearForm()
-{
-productNameInput.value = null;
-productPriceInput.value = null;
-productCategoryInput.value = null;
-productDescriptionInput.value = null;
-productImageInput.value = null;
-}
-//_______________________________________________________________________________
 
-function displayProducts()
+
+function clearForm() {
+    productNameInput.value = null; 
+    productPriceInput.value = null;
+    productCategoryInput.value = null;
+    productDescriptionInput.value = null;
+    productImageInput.value = null;
+}
+
+
+function displayProducts() 
 {
     var cartona = ``;
-    for( var i=0; i<productContainer.length; i++)
+    for (var i = 0; i < productContainer.length; i++)
     {
-        cartona +=
-        `<div class="col-md-2 col-sm-6">
-            <div class="product">
-            <img src="img/iphone 15.png" class="w-100" alt="iphone">
-            <h2 class="h4 mt-3">${productContainer[i].code.toString()}</h2>
-            <p class="text-secondary mb-2">${productContainer[i].description.toString()}</p>
-            <h3 class="h5"> <span class="fw-bolder">Price:</span>${productContainer[i].price.toString()}  </h3>
-            <h3 class="h5"> <span class="fw-bolder">Category:</span> ${productContainer[i].category.toString()} </h3>
-            </div>
-        </div>`;
+        cartona += `<div class="col-md-2">
+                    <div class="product">
+        <img  src="${productContainer[i].image}"  class="w-100" alt="">
+            <h2 class="h6">${productContainer[i].code}</h2>
+            <p>${productContainer[i].description}.</p>
+            <h3 class="h6">${productContainer[i].price}</h3>
+            <h3 class="h6">${productContainer[i].category}</h3>
+            <button onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-sm w-100 my-2" >Delete <i class="fas fa-trash-alt"></i></button>
+            <button onclick="setFormForUpdate(${i})" class="btn btn-outline-warning btn-sm w-100 my-2" >Update <i class="fas fa-pen"></i></button>
+        </div>
+    </div>`
     }
     document.getElementById('rowData').innerHTML = cartona;
 }
-//_______________________________________________________________________________
+
+
+function deleteProduct(deletedIndex)
+{
+    productContainer.splice(deletedIndex,1);
+    displayProducts();
+    localStorage.setItem("products", JSON.stringify(productContainer));
+
+    
+}
+
+
+function searchProducts()
+{
+    var term = searchInput.value;
+    var cartona = ``;
+    for(var i=0; i<productContainer.length;i++)
+    {
+        if(productContainer[i].code.toLowerCase().includes(term.toLowerCase()) == true) 
+        {
+            cartona += `<div class="col-md-2">
+                    <div class="product">
+            <img  src="${productContainer[i].image}"  class="w-100" alt="">
+            <h2 class="h6">${productContainer[i].code}</h2>
+            <p>${productContainer[i].description}.</p>
+            <h3 class="h6">${productContainer[i].price}</h3>
+            <h3 class="h6">${productContainer[i].category}</h3>
+            <button onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-sm w-100 my-2" >Delete <i class="fas fa-trash-alt"></i></button>
+            <button onclick="setFormForUpdate(${i})" class="btn btn-outline-warning btn-sm w-100 my-2" >Update <i class="fas fa-pen"></i></button>
+            </div>
+            </div>`;
+        }
+    }
+    document.getElementById('rowData').innerHTML = cartona;
+}
+
+
+
+
+function setFormForUpdate(i)
+{
+    updatedIndex = i;
+
+    addBtn.classList.add('d-none');
+    updateBtn.classList.remove('d-none');
+    productNameInput.value = productContainer[i].code;
+    productPriceInput.value = productContainer[i].price;
+    productCategoryInput.value = productContainer[i].category;
+    productDescriptionInput.value = productContainer[i].description;
+}
+
+
+function updateProduct()
+{
+    var x = productContainer
+    addBtn.classList.remove('d-none');
+    updateBtn.classList.add('d-none');
+    productContainer[updatedIndex].code = productNameInput.value;
+    productContainer[updatedIndex].price = productPriceInput.value;
+    productContainer[updatedIndex].description = productDescription.value;
+    productContainer[updatedIndex].category = productCategoryInput.value;
+    
+    displayProducts();
+    localStorage.setItem("products", JSON.stringify(productContainer));
+    clearForm();
+
+    
+}
 
